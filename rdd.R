5f58539a473dbe33723712910789795362e4b9ca
gustavo-tm <gtheilm@gmail.com>
2024-10-21 13:38:17 -0300

    y = densidade

--- rdd.R
+++ rdd.R
@@ -1,24 +1,33 @@
 library(tidyverse)
 library(rdrobust)
-
-
-distancias.2010 |> 
-  st_drop_geometry() |> 
-  filter(abs(distancia_geometria) < 200) |> 
-  (\(df) rdplot(df$moradores, df$distancia_geometria, ci = .99)$rdplot + ylim(c(0,1000)))(df = _) 
+library(sf)
 
 distancias.2022 |> 
-  st_drop_geometry() |> 
-  filter(abs(distancia_geometria) < 200) |> 
-  (\(df) rdplot(df$moradores, df$distancia_geometria, ci = .95)$rdplot + ylim(c(0,1000)))(df = _)
+  filter(abs(distancia_geometria) < 100) |> 
+  summarize(controle = sum(distancia_centroide < 0),
+            tratamento = sum(distancia_centroide > 0))
 
 
-distancias.2010 |> 
-  st_drop_geometry() |> 
-  filter(abs(distancia_geometria) < 200) |> 
-  (\(df) rdplot(as.numeric(df$area_precut), df$distancia_centroide, ci = .99)$rdplot)(df = _) 
 
-distancias.2022 |> 
+rdd.2010 <- distancias.2010 |> 
+  st_drop_geometry() |>
+  mutate(densidade = moradores / as.numeric(area_postcut)) |> 
+  drop_na() |>
+  filter(abs(distancia_geometria) < 500, as.numeric(area_postcut) > 10) |>
+  (\(df) rdplot(as.numeric(df$densidade), df$distancia_geometria, 
+                binselect = "es", nbins = c(10, 10), ci = .99))(df = _) 
+rdd.2010
+rdd.2010$rdplot
+
+rdd.2022 <- distancias.2022 |> 
   st_drop_geometry() |> 
-  filter(abs(distancia_geometria) < 200) |> 
-  (\(df) rdplot(as.numeric(df$area_precut), df$distancia_centroide, ci = .95)$rdplot)(df = _)
+  mutate(densidade = moradores / as.numeric(area_postcut)) |> 
+  drop_na() |>
+  filter(abs(distancia_geometria) < 500, as.numeric(area_postcut) > 10) |>
+  (\(df) rdplot(as.numeric(df$densidade), df$distancia_geometria, 
+                binselect = "es", nbins = c(10, 10), ci = .99))(df = _)
+
+rdd.2022
+rdd.2022$rdplot
+
+
