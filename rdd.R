f94ae7f15c99091ed77f225dcea4fa9535263c81
Gustavo Theil <gtheilm@gmail.com>
2024-10-24 17:33:57 -0300

    tentativa novo X

--- rdd.R
+++ rdd.R
@@ -10,7 +10,7 @@ readRDS("cache/2010-distancias.RDS") |>
   drop_na() |>
   filter(abs(distancia_geometria) < 500, as.numeric(area_postcut) > 10) |>
   (\(df) rdplot(as.numeric(df$densidade), df$distancia_geometria, 
-                binselect = "qs", ci = .99, p = 1))(df = _) 
+                binselect = "es", ci = .99, p = 1, nbins = c(5,5)))(df = _) 
 
 readRDS("cache/2022-distancias.RDS") |> 
   st_drop_geometry() |> 
@@ -18,7 +18,7 @@ readRDS("cache/2022-distancias.RDS") |>
   drop_na() |>
   filter(abs(distancia_geometria) < 500, as.numeric(area_postcut) > 10) |>
   (\(df) rdplot(as.numeric(df$densidade), df$distancia_geometria, 
-                binselect = "qs", ci = .99, p = 1))(df = _)
+                binselect = "es", ci = .99, p = 1, nbins = c(5,5)))(df = _)
 
 # RDROBUST -----
 
@@ -39,7 +39,7 @@ readRDS("cache/2022-distancias.RDS") |>
   drop_na() |>
   filter(abs(distancia_geometria) < 500, as.numeric(area_postcut) > 100) |> 
   (\(df) rdrobust(as.numeric(df$densidade), df$distancia_geometria, 
-                  kernel = "tri", p = 1))(df = _) |> 
+                  kernel = "tri", p = 1, c = 0))(df = _) |> 
   summary()
 
 
